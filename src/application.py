7a30381f3e6e7221f9327c87df2be6e2521633fb
Gizeli Fonseca <gizeli.fonseca@fatec.sp.gov.br>
2021-11-27 20:59:53 -0300

    Troca nome arquivo app

--- src/application.py
+++ src/application.py
@@ -1,23 +1,23 @@
 from flask import app, Flask, render_template
-app= Flask(__name__)
+minha_app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
+@minha_app.route("/")
+@minha_app.route("/index")
 def  home ():
     return render_template ("home.html")
 
-@app.route ("/curiosidades")
+@minha_app.route ("/curiosidades")
 def  curiosidades ():
     return render_template ("curiosidades.html")
 
-@app.route ("/curriculum")
+@minha_app.route ("/curriculum")
 def  curriculum ():
     return render_template ("curriculum.html")
 
 
-@app.route ("/projetos")
+@minha_app.route ("/projetos")
 def  projetos ():
     return render_template ("projetos.html")
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    minha_app.run('0.0.0.0')
